98581b2fe1da33bf515d64612b9639727b1f7e36
crickleaze <ian@crickles.org>
2024-04-14 21:39:00 +0100

    Updated maps with recent rides

--- map_utilities.R
+++ map_utilities.R
@@ -22,3 +22,46 @@ map_polylines <- function(polylines) {
   }
   return(base)
 }
+
+# how_far_from_home --------------------------------------------
+how_far_from_home <- function(polyline) {
+  
+  polyline_to_df(polyline) -> df
+  dist <- haversine(mean(df$lat), mean(df$lon), 50.8964, -3.03738)
+  return(dist)
+}
+
+# haversine -----------------------------------------------------------
+
+haversine <- function(lat1, lon1, lat2, lon2) {
+  # Earth's radius in kilometers
+  R <- 6371
+  
+  # Convert degrees to radians
+  phi1 <- lat1 * (pi / 180)
+  phi2 <- lat2 * (pi / 180)
+  delta_phi <- (lat2 - lat1) * (pi / 180)
+  delta_lambda <- (lon2 - lon1) * (pi / 180)
+  
+  # Haversine formula
+  a <- sin(delta_phi / 2)^2 + cos(phi1) * cos(phi2) * sin(delta_lambda / 2)^2
+  c <- 2 * atan2(sqrt(a), sqrt(1 - a))
+  
+  # Distance in kilometers
+  d <- R * c
+  
+  return(d)
+}
+
+# pending_df to results ---------------------------------------
+
+pending_to_results <- function(pending_df) {
+  library(tidyverse)
+  pending_df |> 
+    filter(athlete.id == 301194, map.polyline != "") |> 
+    rowwise() |> 
+    mutate(km_away = how_far_from_home(map.polyline)) |> 
+    filter(km_away <= 100) |> 
+    select(type, map.polyline, km_away) -> results
+  return(results)
+}
